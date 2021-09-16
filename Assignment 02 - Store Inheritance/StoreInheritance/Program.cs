202685a52e70dc6cdfc84fe3c4ef8e0c23a0fa61
Giuliana Bouzon <ga@bouzon.com.br>
2021-09-16 12:01:36 -0400

    updated readme

---
+++ Assignment 02 - Store Inheritance/StoreInheritance/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreInheritance
+{
+    class Program
+    {
+        //method to get a valid answer for mailing list option aka yes or no
+        public static bool RetrieveMailingListOption()
+        {
+            bool isInMailingList = false;
+            bool isValidOption;
+            string mailingList;
+
+            do
+            {
+                Console.WriteLine("Please enter 'Y' if you would like to be on the mailing list and 'N' if otherwise");
+                mailingList = Console.ReadLine();
+                if (mailingList.ToUpper().Equals("Y") || mailingList.ToUpper().Equals("YES"))
+                {
+                    isValidOption = true;
+                    isInMailingList = true;
+                }
+                else if (mailingList.ToUpper().Equals("N") || mailingList.ToUpper().Equals("NO"))
+                {
+                    isValidOption = true;
+                    isInMailingList = false;
+                }
+                else
+                    isValidOption = false;
+                
+            } while (!isValidOption);
+
+            return isInMailingList;
+        }
+
+        static void Main(string[] args)
+        {
+            //getting user's first name
+            Console.WriteLine("Please enter your first name: ");
+            String firstName = Console.ReadLine();
+
+            //getting user's last name
+            Console.WriteLine("Please enter your last name: ");
+            String lastName = Console.ReadLine();
+
+            //getting user's address
+            Console.WriteLine("Please enter your address: ");
+            String address = Console.ReadLine();
+
+            //getting user's phone number
+            Console.WriteLine("Please enter your phone number: ");
+            String phoneNumber = Console.ReadLine();
+
+            //getting user's customer number
+            Console.WriteLine("Please enter your customer number: ");
+            int customerNumber = Convert.ToInt32(Console.ReadLine());
+
+            //getting user mailing list thingy
+            bool isInMailingList = RetrieveMailingListOption();
+
+            //getting total purchase amount
+            Console.WriteLine("Please enter your total purchase amount: ");
+            decimal purchaseAmount = Convert.ToDecimal(Console.ReadLine());
+
+            //creating Person object from entered information
+            Person person = new Person(lastName, firstName, address, phoneNumber);
+            person.DisplayData();
+
+            //creating Customer object from entered information
+            Customer customer = new Customer(lastName, firstName, address, phoneNumber, customerNumber, isInMailingList);
+            customer.DisplayData();
+
+            //creating PreferredCustomer object from entered info
+            PreferredCustomer customer2 = new PreferredCustomer(lastName, firstName, address, phoneNumber, customerNumber, isInMailingList, purchaseAmount);
+            customer2.DisplayData();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,7 +3,20 @@ Fall 2021. Repository for labs, assignments & projects in Application Developmen
 
 ## Folders
 
-### 1. Lab 1:
+### 1. Assignment 01 - Health Profile:
+
+<p>
+    Package <b>HealthProfile</b> contains my solution for Assignment 1, Part 2 (BMI). <br>
+    Package <b>HeartRates</b> contains my solution for Assignment 1, Part 1. <br>
+</p>
+
+### 2. Assignment 02 - Store Inheritance:
+
+<p>
+    Package <b>StoreInheritance</b> contains my solution for Assignment 2 (Person, Customer, PreferredCustomer). <br>
+</p>
+
+### 3. Lab 1:
 
 <p>
     Package <b>ArithmeticOperations</b> contains the solution for Lab 1 (copy paste lab). <br>
@@ -11,7 +24,7 @@ Fall 2021. Repository for labs, assignments & projects in Application Developmen
     Package <b>FirstClass</b> contains my solution for Lab 1 (copy paste lab). <br>
 </p>
 
-### 2. Notes:
+### 4. Notes:
 
 <p> 
     Contains my notes for C# Programming.
