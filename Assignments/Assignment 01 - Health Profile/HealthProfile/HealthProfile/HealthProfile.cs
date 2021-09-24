293783f51379d3faddfdbbd37f9e75c0711a3162
Giuliana Bouzon <ga@bouzon.com.br>
2021-09-24 19:04:03 -0400

    reorganized folders & added lab 2

---
+++ Assignments/Assignment 01 - Health Profile/HealthProfile/HealthProfile/HealthProfile.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthProfile
+{
+    class HealthProfile
+    {
+        //attributes
+        private String firstName;
+        private String lastName;
+        private int birthYear;
+        private int currentYear;
+        private double height;
+        private double weight;
+
+        //constructor
+        public HealthProfile(String firstName, String lastName, int birthYear, double weight, double height)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthYear = birthYear;
+            CurrentYear = currentYear;
+            Weight = weight;
+            Height = height;
+        }
+
+        //property that calculates the age
+        public int Age
+        {
+            get
+            {
+                return currentYear - birthYear;
+            }
+        }
+
+        //property that calculates max heart rate
+        public int MaxHeartRate
+        {
+            get
+            {
+                return 220 - Age;
+            }
+        }
+
+        //property that calculates minimum target heart rate
+        public int MinTargetRate
+        {
+            get
+            {
+                return (int) (0.5 * MaxHeartRate);
+            } 
+        }
+
+        //property that calculates maximum target heart rate
+        public int MaxTargetRate
+        {
+            get
+            {
+                return (int) (0.85 * MaxHeartRate);
+            }
+        }
+
+        //method to calculate BMI
+        public double CalculateBMI()
+        {
+            return Math.Round((weight * 703) / (Math.Pow(height, 2)), 2);
+        }
+
+        public String GetVerbalRating()
+        {
+            double bmi = CalculateBMI();
+
+            if (bmi < 18.5)
+                return "Underweight";
+            else if (bmi < 25)
+                return "Normal";
+            else if (bmi < 30)
+                return "Overweight";
+            else
+                return "Obese";
+        }
+
+        //method to display patient data in tabular format
+        public void DisplayPatientRecord()
+        {
+            Console.WriteLine($"|--------------------------------------------------|");
+            Console.WriteLine($"|            PATIENT HEART RATE RECORD             |");
+            Console.WriteLine($"|--------------------------------------------------|");
+            Console.WriteLine(String.Format($"| {"Patient Name", -20} | {LastName + ", " + FirstName, -25} |"));
+            Console.WriteLine(String.Format($"| {"Patient Birth Year", -20} | {BirthYear, 25} |"));
+            Console.WriteLine(String.Format($"| {"Patient Age", -20} | {Age, 25} |"));
+            Console.WriteLine(String.Format($"| {"Patient Height (in)", -20} | {Height, 25} |"));
+            Console.WriteLine(String.Format($"| {"Patient Weight (lbs)", -20} | {Weight, 25} |"));
+            Console.WriteLine(String.Format($"| {"Maximum Heart Rate", -20} | {MaxHeartRate, 25} |"));
+            Console.WriteLine(String.Format($"| {"Target Heart Rate", -20} | {MinTargetRate + " - " + MaxTargetRate, 25} |"));
+            Console.WriteLine(String.Format($"| {"BMI Numeric Value", -20} | {CalculateBMI(), 25} |"));
+            Console.WriteLine(String.Format($"| {"BMI Text Value", -20} | {GetVerbalRating(), -25} |"));
+            Console.WriteLine($"|--------------------------------------------------|");
+        }
+
+        //properties
+        public String FirstName
+        {
+            get 
+            { 
+                return firstName; 
+            }
+
+            set 
+            {
+                firstName = value;
+            }
+        }
+
+        public String LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public int BirthYear
+        {
+            get
+            {
+                return birthYear;
+            }
+            set
+            {
+                birthYear = (value > 0) ? value : 0;
+            }
+        }
+
+        public int CurrentYear
+        {
+            get
+            {
+                return currentYear;
+            }
+            set
+            {
+                currentYear = DateTime.Now.Year;
+            }
+        }
+
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = (value > 0) ? value : 0;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = (value > 0) ? value : 0;
+            }
+        }
+    }
+
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            //getting user's first name
+            Console.WriteLine("Please enter your first name: ");
+            String firstName = Console.ReadLine();
+
+            //getting user's last name
+            Console.WriteLine("Please enter your last name: ");
+            String lastName = Console.ReadLine();
+
+            //getting user's year of birth
+            Console.WriteLine("Please enter your year of birth: ");
+            int birthYear = Convert.ToInt32(Console.ReadLine());
+
+            //getting user's height in inches
+            Console.WriteLine("Please enter your height in inches: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            //getting user's weight in pounds
+            Console.WriteLine("Please enter your weight in pounds: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            //creating HealthProfile object from entered information
+            HealthProfile h2 = new HealthProfile(firstName, lastName, birthYear, weight, height);
+            h2.DisplayPatientRecord();
+        }
+    }
+}
---
+++ Labs/Lab 02 - LINQ/LINQWithArrayOfObjects/LINQWithArrayOfObjects/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQWithArrayOfObjects
+{
+
+    class Employee
+    {
+        public string FirstName { get; } // read-only auto-implemented property
+        public string LastName { get; } // read-only auto-implemented property
+        private decimal monthlySalary; // monthly salary of employee
+
+        // constructor initializes first name, last name and monthly salary
+        public Employee(string firstName, string lastName, decimal monthlySalary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MonthlySalary = monthlySalary;
+        } 
+
+        // property that gets and sets the employee's monthly salary
+        public decimal MonthlySalary
+        {
+            get
+            {
+                return monthlySalary;
+            }
+            set
+            {
+                if (value >= 0M) // validate that salary is nonnegative
+                {
+                    monthlySalary = value;
+                }
+            }
+        } 
+
+        // return a string containing the employee's information
+        public override string ToString() =>
+            $"{FirstName,-10} {LastName,-10} {MonthlySalary,10:C}";
+    }
+    class LINQWithArrayOfObjects
+    {
+        static void Main(string[] args)
+        {
+            // initialize array of employees
+            var employees = new[] {
+                new Employee("Jason", "Red", 5000M),
+                new Employee("Ashley", "Green", 7600M),
+                new Employee("Matthew", "Indigo", 3587.5M),
+                new Employee("James", "Indigo", 4700.77M),
+                new Employee("Luke", "Indigo", 6200M),
+                new Employee("Jason", "Blue", 3200M),
+                new Employee("Wendy", "Brown", 4236.4M)};
+            
+            // display all employees
+            Console.WriteLine("Original array:");
+            foreach (var element in employees)
+            {
+                Console.WriteLine(element);
+            }
+
+            // filter a range of salaries using && in a LINQ query
+            var between4K6K =
+                from e in employees
+                where (e.MonthlySalary >= 4000M) && (e.MonthlySalary <= 6000M)
+                select e;
+
+            // display employees making between 4000 and 6000 per month
+            Console.WriteLine("\nEmployees earning in the range" +
+                $"{4000:C}-{6000:C} per month:");
+            foreach (var element in between4K6K)
+            {
+                Console.WriteLine(element);
+            }
+
+            // order the employees by last name, then first name with LINQ
+            var nameSorted =
+                from e in employees
+                orderby e.LastName, e.FirstName
+                select e;
+
+            // header
+            Console.WriteLine("\nFirst employee when sorted by name:");
+ 
+            // attempt to display the first result of the above LINQ query
+            if (nameSorted.Any())
+            {
+                Console.WriteLine(nameSorted.First());
+            }
+            else
+            {
+                Console.WriteLine("not found");
+            }
+
+            // use LINQ to select employee last names
+            var lastNames =
+                from e in employees
+                select e.LastName;
+ 
+            // use method Distinct to select unique last names
+            Console.WriteLine("\nUnique employee last names:");
+            foreach (var element in lastNames.Distinct())
+            {
+                Console.WriteLine(element);
+            }
+
+            // use LINQ to select first and last names
+            var names =
+                from e in employees
+                select new { e.FirstName, e.LastName }; //Anonymous class object
+
+            // display full names
+            Console.WriteLine("\nNames only:");
+            foreach (var element in names)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine();
+        }
+    }
+}
