278375ea5856d2cb7577971a67295003486ec863
Giuliana Bouzon <ga@bouzon.com.br>
2021-10-06 03:50:11 -0400

    added lab 4, project 12 (user control)

---
+++ Labs/Lab 04 - Base GUI Applications 2/ClockExample/ClockUserControl/ClockUserControl.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace ClockExample
+{
+    partial class ClockUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.displayLabel = new System.Windows.Forms.Label();
+            this.clockTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // displayLabel
+            // 
+            this.displayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayLabel.Location = new System.Drawing.Point(52, 38);
+            this.displayLabel.Name = "displayLabel";
+            this.displayLabel.Size = new System.Drawing.Size(100, 36);
+            this.displayLabel.TabIndex = 0;
+            this.displayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clockTimer
+            // 
+            this.clockTimer.Enabled = true;
+            this.clockTimer.Interval = 1000;
+            this.clockTimer.Tick += new System.EventHandler(this.clockTimer_Tick);
+            // 
+            // ClockUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.displayLabel);
+            this.Name = "ClockUserControl";
+            this.Size = new System.Drawing.Size(205, 105);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label displayLabel;
+        private System.Windows.Forms.Timer clockTimer;
+    }
+}
