5e9bcbd05161658d6c99297cbb07ce97ace44855
Giuliana Bouzon <ga@bouzon.com.br>
2021-11-07 23:18:27 -0500

    added lab 6, project 3

---
+++ Labs/Lab 05 - File & Streams Processing/CreditInquiry/CreditInquiry/CreditInquiryForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace CreditInquiry
+{
+    partial class CreditInquiryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayTextBox = new System.Windows.Forms.RichTextBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.creditButton = new System.Windows.Forms.Button();
+            this.debitButton = new System.Windows.Forms.Button();
+            this.zeroButton = new System.Windows.Forms.Button();
+            this.doneButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayTextBox
+            // 
+            this.displayTextBox.Location = new System.Drawing.Point(29, 29);
+            this.displayTextBox.Name = "displayTextBox";
+            this.displayTextBox.Size = new System.Drawing.Size(686, 170);
+            this.displayTextBox.TabIndex = 1;
+            this.displayTextBox.Text = "";
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(29, 220);
+            this.openButton.Name = "openButton";
+            this.openButton.Enabled = true;
+            this.openButton.Size = new System.Drawing.Size(110, 46);
+            this.openButton.TabIndex = 2;
+            this.openButton.Text = "Open File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // creditButton
+            // 
+            this.creditButton.Location = new System.Drawing.Point(163, 220);
+            this.creditButton.Name = "creditButton";
+            this.creditButton.Enabled = false;
+            this.creditButton.Size = new System.Drawing.Size(121, 46);
+            this.creditButton.TabIndex = 3;
+            this.creditButton.Text = "Credit Balances";
+            this.creditButton.UseVisualStyleBackColor = true;
+            this.creditButton.Click += new System.EventHandler(this.getBalances_Click);
+            // 
+            // debitButton
+            // 
+            this.debitButton.Location = new System.Drawing.Point(313, 220);
+            this.debitButton.Name = "debitButton";
+            this.debitButton.Enabled = false;
+            this.debitButton.Size = new System.Drawing.Size(116, 46);
+            this.debitButton.TabIndex = 4;
+            this.debitButton.Text = "Debit Balances";
+            this.debitButton.UseVisualStyleBackColor = true;
+            this.debitButton.Click += new System.EventHandler(this.getBalances_Click);
+            // 
+            // zeroButton
+            // 
+            this.zeroButton.Location = new System.Drawing.Point(457, 220);
+            this.zeroButton.Name = "zeroButton";
+            this.zeroButton.Enabled = false;
+            this.zeroButton.Size = new System.Drawing.Size(119, 46);
+            this.zeroButton.TabIndex = 5;
+            this.zeroButton.Text = "Zero Balances";
+            this.zeroButton.UseVisualStyleBackColor = true;
+            this.zeroButton.Click += new System.EventHandler(this.getBalances_Click);
+            // 
+            // doneButton
+            // 
+            this.doneButton.Location = new System.Drawing.Point(604, 220);
+            this.doneButton.Name = "doneButton";
+            this.doneButton.Enabled = true;
+            this.doneButton.Size = new System.Drawing.Size(111, 46);
+            this.doneButton.TabIndex = 6;
+            this.doneButton.Text = "Done";
+            this.doneButton.UseVisualStyleBackColor = true;
+            this.doneButton.Click += new System.EventHandler(this.doneButton_Click);
+            // 
+            // CreditInquiryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 292);
+            this.Controls.Add(this.doneButton);
+            this.Controls.Add(this.zeroButton);
+            this.Controls.Add(this.debitButton);
+            this.Controls.Add(this.creditButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.displayTextBox);
+            this.Name = "CreditInquiryForm";
+            this.Text = "Credit Inquiry";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox displayTextBox;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button creditButton;
+        private System.Windows.Forms.Button debitButton;
+        private System.Windows.Forms.Button zeroButton;
+        private System.Windows.Forms.Button doneButton;
+    }
+}
+
---
+++ Labs/Lab 05 - File & Streams Processing/CreditInquiry/CreditInquiry/CreditInquiryForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using BankLibrary;
+
+namespace CreditInquiry
+{
+    public partial class CreditInquiryForm : Form
+    {
+        private FileStream input;
+        private StreamReader fileReader;
+        public CreditInquiryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            string fileName;
+
+            using (OpenFileDialog fileChooser = new OpenFileDialog())
+            {
+                result = fileChooser.ShowDialog();
+                fileName = fileChooser.FileName;
+            }
+
+            if (result == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(fileName))
+                    MessageBox.Show("Invalid File Name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    input = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                    fileReader = new StreamReader(input);
+                    openButton.Enabled = false;
+                    creditButton.Enabled = true;
+                    debitButton.Enabled = true;
+                    zeroButton.Enabled = true;
+                }
+            }
+        }
+
+        private void getBalances_Click(object sender, EventArgs e)
+        {
+            Button senderButton = (Button) sender;
+            string accountType = senderButton.Text;
+            try
+            {
+                input.Seek(0, SeekOrigin.Begin);
+                displayTextBox.Text = $"Accounts with {accountType}{Environment.NewLine}";
+
+                while (true)
+                {
+                    string inputRecord = fileReader.ReadLine();
+
+                    if (inputRecord == null)
+                        return;
+
+                    string[] inputFields = inputRecord.Split(',');
+                    var record = new Record(int.Parse(inputFields[0]), inputFields[1], inputFields[2],
+                        decimal.Parse(inputFields[3]));
+
+                    if (ShouldDisplay(record.Balance, accountType))
+                        displayTextBox.AppendText($"{record.Account}\t" + 
+                            $"{record.FirstName}\t{record.LastName}\t" + 
+                            $"{record.Balance:C}{Environment.NewLine}");
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Cannot Read File", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ShouldDisplay(decimal balance, string accountType)
+        {
+            if (balance > 0M && accountType == "Credit Balances")
+                return true;
+
+            else if (balance < 0M && accountType == "Debit Balances")
+                return true;
+
+            else if (balance == 0 && accountType == "Zero Balances")
+                return true;
+
+            return false;
+        }
+
+        private void doneButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                fileReader?.Close();
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Cannot close file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Application.Exit();
+        }
+    }
+}
