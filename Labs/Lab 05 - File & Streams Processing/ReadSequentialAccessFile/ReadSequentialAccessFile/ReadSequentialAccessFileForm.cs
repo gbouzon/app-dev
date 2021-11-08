3beac8b4a659a64e515783159c759f19d10265b5
Giuliana Bouzon <ga@bouzon.com.br>
2021-11-07 21:54:21 -0500

    added lab 5, project 2

---
+++ Labs/Lab 05 - File & Streams Processing/ReadSequentialAccessFile/ReadSequentialAccessFile/ReadSequentialAccessFileForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BankLibrary;
+using System.IO;
+
+namespace ReadSequentialAccessFile
+{
+    public partial class ReadSequentialAccessFileForm : BankUIForm
+    {
+        private StreamReader fileReader;
+        public ReadSequentialAccessFileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            string fileName;
+
+            using (OpenFileDialog fileChooser = new OpenFileDialog())
+            {
+                result = fileChooser.ShowDialog();
+                fileName = fileChooser.FileName;
+            }
+
+            if (result == DialogResult.OK) 
+            {
+                ClearTextBoxes();
+                if (string.IsNullOrEmpty(fileName))
+                    MessageBox.Show("Invalid File Name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    try
+                    {
+                        FileStream input = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                        fileReader = new StreamReader(input);
+                        openButton.Enabled = false;
+                        openButton.Enabled = true;
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error reading from file", "File Error", MessageBoxButtons.OK, 
+                            MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var inputRecord = fileReader.ReadLine();
+
+                if (inputRecord != null)
+                {
+                    string[] inputFields = inputRecord.Split(',');
+                    SetTextBoxValues(inputFields);
+                }
+                else
+                {
+                    fileReader.Close();
+                    openButton.Enabled = true;
+                    nextButton.Enabled = false;
+                    ClearTextBoxes();
+                    MessageBox.Show("No more records in file", string.Empty, MessageBoxButtons.OK,
+                           MessageBoxIcon.Information);
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Error reading from file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
