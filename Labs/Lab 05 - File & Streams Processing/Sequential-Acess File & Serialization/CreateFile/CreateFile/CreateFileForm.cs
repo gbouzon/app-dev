8dc238cb36ac8689d95cf29e112f24f98b29f68c
Giuliana Bouzon <ga@bouzon.com.br>
2021-11-07 23:43:26 -0500

    added lab 5, project 4

---
+++ Labs/Lab 05 - File & Streams Processing/Sequential-Acess File & Serialization/CreateFile/CreateFile/CreateFileForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BankLibrary;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace CreateFile
+{
+    public partial class CreateFileForm : BankUIForm
+    {
+        private BinaryFormatter formatter = new BinaryFormatter();
+        private FileStream output;
+        public CreateFileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            string fileName;
+
+            using (var fileChooser = new SaveFileDialog())
+            {
+                fileChooser.CheckFileExists = false;
+                result = fileChooser.ShowDialog();
+                fileName = fileChooser.FileName;
+            }
+
+            if (result == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(fileName))
+                    MessageBox.Show("Invalid File Name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                else
+                {
+                    try
+                    {
+                        output = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+                        saveButton.Enabled = false;
+                        enterButton.Enabled = true;
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error opening file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            string[] values = GetTextBoxValues();
+
+            if (!string.IsNullOrEmpty(values[(int)TextBoxIndices.Account]))
+            {
+                try
+                {
+                    int accountNumber = int.Parse(values[(int)TextBoxIndices.Account]);
+                    if (accountNumber > 0)
+                    {
+                        var record = new RecordSerializable(accountNumber, values[(int)TextBoxIndices.First],
+                            values[(int)TextBoxIndices.Last], decimal.Parse(values[(int)TextBoxIndices.Balance]));
+
+                        formatter.Serialize(output, record);
+                    }
+                    else
+                        MessageBox.Show("Invalid Account Number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                catch (SerializationException)
+                {
+                    MessageBox.Show("Error Writing to File", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Invalid Format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            ClearTextBoxes();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                output?.Close();
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Cannot close file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Application.Exit();
+        }
+    }
+}
