980853dc6a4aeaccf11fa93019e2f74d36d73880
Giuliana Bouzon <ga@bouzon.com.br>
2021-11-09 23:07:15 -0500

    added lab 5, project 7

---
+++ Labs/Lab 05 - File & Streams Processing/LINQToFileDirectory/LINQToFileDirectory/LINQToFileDirectoryForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace LINQToFileDirectory
+{
+    partial class LINQToFileDirectoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.directoryTextBox = new System.Windows.Forms.TextBox();
+            this.resultsTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(23, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(349, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter path to search:";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(26, 53);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(346, 22);
+            this.pathTextBox.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(200, 92);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(172, 32);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search Directory";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(34, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Directory:";
+            // 
+            // directoryTextBox
+            // 
+            this.directoryTextBox.Location = new System.Drawing.Point(111, 140);
+            this.directoryTextBox.Name = "directoryTextBox";
+            this.directoryTextBox.Size = new System.Drawing.Size(261, 22);
+            this.directoryTextBox.TabIndex = 4;
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Location = new System.Drawing.Point(37, 183);
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.resultsTextBox.Size = new System.Drawing.Size(335, 256);
+            this.resultsTextBox.TabIndex = 5;
+            this.resultsTextBox.Text = "";
+            // 
+            // LINQToFileDirectoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 473);
+            this.Controls.Add(this.resultsTextBox);
+            this.Controls.Add(this.directoryTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "LINQToFileDirectoryForm";
+            this.Text = "Search Directories";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox directoryTextBox;
+        private System.Windows.Forms.RichTextBox resultsTextBox;
+    }
+}
+
---
+++ Labs/Lab 05 - File & Streams Processing/LINQToFileDirectory/LINQToFileDirectory/LINQToFileDirectoryForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LINQToFileDirectory
+{
+    public partial class LINQToFileDirectoryForm : Form
+    {
+        Dictionary<string, int> found = new Dictionary<string, int>();
+
+        public LINQToFileDirectoryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(pathTextBox.Text) && !Directory.Exists(pathTextBox.Text))
+                MessageBox.Show("Invalid Directory", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            else
+            {
+                string currentDirectory = (!string.IsNullOrEmpty(pathTextBox.Text)) ? pathTextBox.Text :
+                    Directory.GetCurrentDirectory();
+
+                directoryTextBox.Text = currentDirectory;
+
+                pathTextBox.Clear();
+                resultsTextBox.Clear();
+
+                SearchDirectory(currentDirectory);
+
+                CleanDirectory(currentDirectory);
+
+                foreach (var current in found.Keys)
+                    resultsTextBox.AppendText($"* Found {found[current]} {current} files." + Environment.NewLine);
+
+                found.Clear();
+
+            }
+        }
+
+        private void SearchDirectory(string folder)
+        {
+            string[] files = Directory.GetFiles(folder);
+
+            string[] directories = Directory.GetDirectories(folder);
+
+            var extensions =
+                from file in files
+                group file by Path.GetExtension(file);
+
+            foreach (var extension in extensions)
+            {
+                if (found.ContainsKey(extension.Key))
+                    found[extension.Key] += extension.Count();
+                else
+                    found[extension.Key] = extension.Count();
+            }
+
+            foreach (var subDirectory in directories)
+                SearchDirectory(subDirectory);
+        }
+
+        private void CleanDirectory(string folder)
+        {
+            string[] files = Directory.GetFiles(folder);
+
+            string[] directories = Directory.GetDirectories(folder);
+
+            var backupFiles =
+                from file in files
+                where Path.GetExtension(file) == ".bak"
+                select file;
+
+            foreach (var backup in backupFiles)
+            {
+                DialogResult result = MessageBox.Show($"Found backup file {Path.GetFileName(backup)}. Delete?",
+                    "Delete Backup", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    File.Delete(backup);
+                    --found[".bak"];
+
+                    if (found[".bak"] == 0)
+                        found.Remove(".bak");
+                }
+            }
+
+            foreach (var subdirectory in directories)
+                CleanDirectory(subdirectory);
+        }
+    }
+}
