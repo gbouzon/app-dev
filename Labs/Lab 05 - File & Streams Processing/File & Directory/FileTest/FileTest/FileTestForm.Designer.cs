ba0546cb9a24442878782234ea112c239806018b
Giuliana Bouzon <ga@bouzon.com.br>
2021-11-09 22:24:45 -0500

    added lab 5, project 6

---
+++ Labs/Lab 05 - File & Streams Processing/File & Directory/FileTest/FileTest/FileTestForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace FileTest
+{
+    partial class FileTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(24, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter file or directory:";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(27, 55);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(340, 22);
+            this.inputTextBox.TabIndex = 1;
+            this.inputTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputTextBox_KeyDown);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(27, 101);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.outputTextBox.Size = new System.Drawing.Size(340, 326);
+            this.outputTextBox.TabIndex = 3;
+            this.outputTextBox.Text = "";
+            // 
+            // FileTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 454);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "FileTestForm";
+            this.Text = "File Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+    }
+}
+
---
+++ Labs/Lab 05 - File & Streams Processing/File & Directory/FileTest/FileTest/FileTestForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileTest
+{
+    public partial class FileTestForm : Form
+    {
+        public FileTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void inputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string fileName = inputTextBox.Text;
+
+                if (File.Exists(fileName))
+                {
+                    GetInformation(fileName);
+
+                    try
+                    {
+                        using (var stream = new StreamReader(fileName))
+                        {
+                            outputTextBox.AppendText(stream.ReadToEnd());
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error reading from file", "File Error", MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                    }
+                }
+                else if (Directory.Exists(fileName))
+                {
+                    GetInformation(fileName);
+
+                    string[] directoryList = Directory.GetDirectories(fileName);
+                    outputTextBox.AppendText("Directory contents:\n");
+
+                    foreach (var directory in directoryList)
+                    {
+                        outputTextBox.AppendText($"{directory}\n");
+                    }
+                }
+                else
+                    MessageBox.Show($"\'{inputTextBox.Text}\' does not exist", "File Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    
+
+        private void GetInformation(string fileName)
+        {
+            outputTextBox.Clear();
+            outputTextBox.AppendText($"\'{fileName}\' exists\n" + Environment.NewLine);
+
+            outputTextBox.AppendText(
+                $"Created: {File.GetCreationTime(fileName)}\n" + Environment.NewLine);
+
+            outputTextBox.AppendText(
+                $"Last modified: {File.GetLastWriteTime(fileName)}\n" + Environment.NewLine);
+
+            outputTextBox.AppendText(
+                $"Last accessed: {File.GetLastAccessTime(fileName)}\n" + Environment.NewLine);
+        }
+    }
+}
