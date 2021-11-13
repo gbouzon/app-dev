ba98408fbb122d98af3ef9b5e6a0c1e32e06fb8c
Giuliana Bouzon <ga@bouzon.com.br>
2021-11-12 19:38:08 -0500

    added lab 6, project 1 & 2 & product mdf

---
+++ Labs/Lab 06 - GUI-based Applications & Databases/LibraryBooks/LibraryBooks/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryBooks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bookBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.bookBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bookDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'bookDataSet.Book' table. You can move, or remove it, as needed.
+            this.bookTableAdapter.Fill(this.bookDataSet.Book);
+
+        }
+    }
+}
